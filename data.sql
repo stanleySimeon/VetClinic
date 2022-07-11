b59afad7faf99c01053293ef2b566dacaae95244
Stanley SIMEON <mstanley.me@gmail.com>
2022-07-11 19:20:24 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,6 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', '02-03-2022', 0, TRUE, 10.23);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '11-15-2018', 2, TRUE, 8);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '01-07-2021', 1, FALSE, 15.04);
-INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4, 'Devimon', '05-12-2017', 5, TRUE, 11);
+INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name) VALUES ('Daisy');
+INSERT INTO animals (name) VALUES ('Charlie');
