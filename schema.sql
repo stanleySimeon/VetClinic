8c16950b1bdf528c73f7908a5678e24a622c32f4
Stanley SIMEON <mstanley.me@gmail.com>
2022-07-12 00:09:07 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+    CREATE TABLE animals (
+       id int,
+       name varchar(255),
+       date_of_birth date,
+       escape_attempts integer,
+       neutered boolean,
+       weight_kg decimal, 
+    );
