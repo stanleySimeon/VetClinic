f3a9c1000bce12258ce8a152ea42070c0ef54d08
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-07-14 18:37:15 +0100

    Update database and queries

--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ CREATE TABLE animals(
     weight_kg DECIMAL
 );
 
-
 -- Add a new column to the table
 ALTER TABLE animals ADD species VARCHAR(255);
 
@@ -34,3 +33,37 @@ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 -- Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+-- Create a table name vets 
+CREATE TABLE vets(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name CHAR(50),
+    age INT NOT NULL,
+    date_of_graduation DATE
+)
+
+
+-- Create a JOIN table specializations to handle this relationship.
+CREATE TABLE specializations(
+    vets_id INT NOT NULL,
+    species_id INT NOT NULL,
+    CONSTRAINT fk_species 
+        FOREIGN KEY (species_id) 
+        REFERENCES species(id),
+    CONSTRAINT fk_vets
+        FOREIGN KEY (vets_id)
+        REFERENCES vets(id)
+)
+
+-- Create a JOIN table visits to handle this relationship by of the date of the visit.
+CREATE TABLE visits(
+    date DATE,
+    vets_id INT NOT NULL,
+    animals_id INT NOT NULL,
+    CONSTRAINT fk_animals
+        FOREIGN KEY (animals_id) 
+        REFERENCES animals(id),
+    CONSTRAINT fk_vets
+        FOREIGN KEY (vets_id)
+        REFERENCES vets(id)
+)
